72b04ab1856f15669b546357bfc913f6e20e5a1b
mrzogy <magbolmrzogy@gmail.com>
2023-06-03 16:19:21 +0300

    Make New Branch

---
+++ SwiftBitCoinMvvm/SwiftBitCoinMvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBitCoinMvvmApp.swift
+//  SwiftBitCoinMvvm
+//
+//  Created by 3bood on 14/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBitCoinMvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
